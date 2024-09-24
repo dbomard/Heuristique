04878ee9b0801605b9d147a04eef3458ab1c8b6c
david bomard <david.bomard@laposte.net>
2024-09-24 15:58:08 +0200

    refactoring

--- js/script.js
+++ js/script.js
@@ -1,4 +1,16 @@
-document.addEventListener('DOMContentLoaded',()=>{
-    console.log('DOM loaded')
-    // const 
-})
+function updateBackYears() {
+    const columns = [];
+    const backYearsSelector = document.querySelector("#back-years");
+    const backYearsCount = backYearsSelector.value
+    
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("DOM loaded");
+  const currentYearInput = document.querySelector("#current-year");
+  console.log(backYearsCount);
+  const Column0 = document.querySelector("#column0");
+  const Column1 = document.querySelector("#column1");
+  const Column2 = document.querySelector("#column2");
+  const Column3 = document.querySelector("#column3");
+});
