a6a4d7178233fd9b5ab55e63cb892506d0a52e99
david bomard <david.bomard@laposte.net>
2024-09-25 18:28:14 +0200

    ajout note refactoring html refactoring js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,20 @@
-function updateBackYears() {
-    const columns = [];
-    const backYearsSelector = document.querySelector("#back-years");
-    const backYearsCount = backYearsSelector.value
-    
+function updateformula() {
+  const column0 = document.querySelector("#column0");
+  const column1 = document.querySelector("#column1");
+  const column2 = document.querySelector("#column2");
+  const column3 = document.querySelector("#column3");
+  const acq = document.querySelector("#acq");
+  const lent = document.querySelector("#lent");
+
+  const formula = document.querySelector("#formula");
+  const innerText = `(${column0.innerText}1*0,5)+()`
 }
 
 document.addEventListener("DOMContentLoaded", () => {
   console.log("DOM loaded");
+  const currentYear = document.querySelector("#current-year");
+  currentYear.innerText = new Date().getFullYear();
+
   const currentYearInput = document.querySelector("#current-year");
   console.log(backYearsCount);
   const Column0 = document.querySelector("#column0");
