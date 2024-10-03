8d6c0b0e5bf0c03004ab6ef119b0018ed38df465
david bomard <david.bomard@laposte.net>
2024-10-03 19:56:08 +0200

    Ajout Formule + copie clipboard

--- js/script.js
+++ js/script.js
@@ -1,24 +1,46 @@
 function updateformula() {
-  const column0 = document.querySelector("#column0");
-  const column1 = document.querySelector("#column1");
-  const column2 = document.querySelector("#column2");
-  const column3 = document.querySelector("#column3");
-  const acq = document.querySelector("#acq");
-  const lent = document.querySelector("#lent");
+  const columnN0 = document.querySelector("#columnN0");
+  const columnN1 = document.querySelector("#columnN1");
+  // const columnN2 = document.querySelector("#columnN2");
+  // const columnN3 = document.querySelector("#columnN3");
+  // const acq = document.querySelector("#acq");
+  const lastLoan = document.querySelector("#lastLoan");
+  const totalLoan = document.querySelector("#totalLoan");
+  const recordYear = document.querySelector("#recordYear");
+  const currentYear = document.querySelector("#current-year").innerText;
+  const nextYear = parseInt(currentYear) + 1;
 
   const formula = document.querySelector("#formula");
-  const innerText = `(${column0.innerText}1*0,5)+()`
+  const text = `=${columnN0.value}2*1+\
+${columnN1.value}2*0,5+\
+(50/(${nextYear}-SI(ANNEE(${recordYear.value}2)="";2010;ANNEE(${recordYear.value}2))))+\
+(20/(${nextYear}-SI(ANNEE(${lastLoan.value}2)="";2000;ANNEE(${lastLoan.value}2))))+\
+(${totalLoan.value}2/(${nextYear}-SI(ANNEE(${recordYear.value}2)="";2010;ANNEE(${recordYear.value}2))))`;
+  formula.innerText = text;
+}
+
+function copyToClipboard() {
+  console.log("Fonction de copie");
+  const formula = document.querySelector("#formula").innerText;
+
+  navigator.clipboard.writeText(formula).then(
+    function () {
+      alert("Le texte a été copié");
+    },
+    function () {
+      alert("Le texte n'a pas été copié");
+    }
+  );
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("DOM loaded");
-  const currentYear = document.querySelector("#current-year");
-  currentYear.innerText = new Date().getFullYear();
-
   const currentYearInput = document.querySelector("#current-year");
-  console.log(backYearsCount);
-  const Column0 = document.querySelector("#column0");
-  const Column1 = document.querySelector("#column1");
-  const Column2 = document.querySelector("#column2");
-  const Column3 = document.querySelector("#column3");
+  currentYearInput.innerText = new Date().getFullYear();
+  const columns = document.querySelectorAll(".entry");
+  columns.forEach((element) =>
+    element.addEventListener("change", updateformula)
+  );
+  updateformula();
+  const copyBtn = document.querySelector("#copy");
+  copyBtn.addEventListener("click", copyToClipboard);
 });
